d8de2910be3bb4f64f31660d577b394cfda8198a
Baymaxqwq <baymaxawa@gmail.com>
2024-10-16 14:53:48 +0800

    3d preview (wip)

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,15 +1,12 @@
 extends Node
 
+var mouse_sensitivity : float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("toggle fullscreen"):
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_size(Vector2i(1152, 864))
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,13 +1,27 @@
 extends Node2D
 
+@onready var slider = $"Title/Mouse/Slider"
+@onready var title = $"Title"
+@onready var howto = $"HowTo"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	title.visible = true
+	howto.visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	Global.mouse_sensitivity = slider.value
+	await get_tree().create_timer(0.2*delta).timeout
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_start_pressed() -> void:
+	pass
+
+func _on_how_to_play_pressed() -> void:
+	title.visible = false
+	howto.visible = true
+
+func _on_back_pressed() -> void:
+	title.visible = true
+	howto.visible = false
