0a2b71439d812fb2f79e25c33e3ef3c01cd0983c
Baymaxawa <2502474438@qq.com>
2024-10-18 23:14:41 +0800

    Add player, movement, camera, debug features(WIP)

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,35 @@ extends Node3D
 @onready var player = $"."
 @onready var camera : Camera3D = $Camera3D
 var mouse_sens = 0.3
-var camera_anglev=0
 
 func _input(event):  		
 	if event is InputEventMouseMotion:
 		camera.rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
-		var changev=-event.relative.y*mouse_sens
-		#if camera_anglev+changev>-50 and camera_anglev+changev<50:
-			#camera_anglev+=changev
-		camera.rotate_x(deg_to_rad(changev))
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("toggle fullscreen"):
-		get_tree().exit()
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+	if EngineDebugger.is_active():
+		if Input.is_action_pressed("forward"):
+			player.position.x += 0.1
+		if Input.is_action_pressed("backward"):
+			player.position.x -= 0.1
+		if Input.is_action_pressed("up"):
+			player.position.y += 0.1
+		if Input.is_action_pressed("down"):
+			player.position.y -= 0.1
+		if Input.is_action_pressed("left"):
+			player.position.z += 0.1
+		if Input.is_action_pressed("right"):
+			player.position.z -= 0.1
+		if Input.is_action_pressed("scroll up"):
+			camera.position.z += 0.1
+			camera.position.y += 0.05
+			camera.position.x += 0.08
+		if Input.is_action_pressed("scroll down"):
+			camera.position.z -= 0.1
+			camera.position.y -= 0.05
+			camera.position.x -= 0.08
